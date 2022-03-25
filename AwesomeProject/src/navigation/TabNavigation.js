2c7d1f0708fbd963dc300c53296b81ff1f30a28b
Mohammad Rada <mohammad@ziphr.co>
2022-03-26 01:23:02 +0300

    refactor and document tab navigation

--- AwesomeProject/src/navigation/TabNavigation.js
+++ AwesomeProject/src/navigation/TabNavigation.js
@@ -5,28 +5,39 @@ import HomeScreen from '@/containers/HomeScreen';
 import Icon from 'react-native-vector-icons/Ionicons';
 import MyOffersScreen from '@/containers/MyOffersScreen';
 import Colors from '@/constants/Colors';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 const Tab = createBottomTabNavigator();
 
+/**
+ * Tab navigation for showing the selected screen.
+ * @returns Tab Navigation selected screen.
+ */
 export default function TabNavigation() {
+  /**
+   * Renders the Icon for a screen.
+   * @param focused If the icon is selected.
+   * @param route The route name of the icon.
+   * @returns The tab icon.
+   */
+  const renderTabIcon = (focused, route) => {
+    let iconName;
+    if (route.name === 'Home') {
+      iconName = focused ? 'home' : 'home-outline';
+    } else if (route.name === 'MyOffers') {
+      iconName = focused ? 'pricetag' : 'pricetag-outline';
+    }
+    return (
+      <View style={styles.iconMargin}>
+        <Icon name={iconName} size={27} color="#900" />
+      </View>
+    );
+  };
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={({ route }) => ({
           headerShown: false,
-          tabBarIcon: ({ focused }) => {
-            let iconName;
-            if (route.name === 'Home') {
-              iconName = focused ? 'home' : 'home-outline';
-            } else if (route.name === 'MyOffers') {
-              iconName = focused ? 'pricetag' : 'pricetag-outline';
-            }
-            return (
-              <View style={{ marginTop: 7 }}>
-                <Icon name={iconName} size={27} color="#900" />
-              </View>
-            );
-          },
+          tabBarIcon: ({ focused }) => renderTabIcon(focused, route),
           tabBarActiveTintColor: Colors.tomato,
           tabBarInactiveTintColor: Colors.gray,
         })}
@@ -37,3 +48,7 @@ export default function TabNavigation() {
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  iconMargin: { marginTop: 7 },
+});
