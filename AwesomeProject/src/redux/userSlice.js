bff44d234a99a9fbdf4fb259e5627d64d284705a
Mohammad Rada <mohammad@ziphr.co>
2022-03-27 13:06:36 +0300

    add user redux slice for user state managemen

---
+++ AwesomeProject/src/redux/userSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+/**
+ * Redux slice for user record.
+ */
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    account: {},
+  },
+  reducers: {
+    setUser: (state, action) => {
+      state.account = action.payload;
+    },
+  },
+});
+
+// ACTIONS export
+export const { setUser } = userSlice.actions;
+export default userSlice.reducer;
