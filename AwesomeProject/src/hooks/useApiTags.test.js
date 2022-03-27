f37a63c93a3734d6e0f3a38685f44154e8195c15
Mohammad Rada <mohammad@ziphr.co>
2022-03-28 02:42:53 +0300

    test the tags query for success, error and timeout test cases

---
+++ AwesomeProject/src/hooks/useApiTags.test.js
@@ -0,0 +1,77 @@
+import useGetTags from '@/hooks/useApiTags';
+import { cleanup, renderHook } from '@testing-library/react-hooks';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+export const server = setupServer();
+// Establish API mocking before all tests.
+beforeAll(() => server.listen());
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers());
+// Clean up after the tests are finished.
+afterAll(() => server.close());
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // ✅ turns retries off
+      retry: false,
+    },
+  },
+});
+
+const wrapper = ({ children }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+describe('useApiTags hook', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+    queryClient.clear();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.clearAllTimers();
+    cleanup();
+  });
+
+  test('successful get tags query should report success', async () => {
+    server.use(rest.get('*', (req, res, ctx) => res(ctx.status(200))));
+    const { result, waitFor } = renderHook(() => useGetTags(), {
+      wrapper,
+    });
+
+    await waitFor(() => result.current.isSuccess);
+
+    expect(result.current.isSuccess).toBeTruthy();
+  });
+
+  test('error get tags query query should report error', async () => {
+    server.use(rest.get('*', (req, res, ctx) => res(ctx.status(500))));
+    const { result, waitFor } = renderHook(() => useGetTags(), {
+      wrapper,
+    });
+
+    await waitFor(() => result.current.isError);
+    expect(result.current.error).toBeTruthy();
+  });
+
+  test('timeout failure get tags query should report a timeout error', async () => {
+    const timeoutDelay = 2000;
+    server.use(
+      rest.get('*', (req, res, ctx) =>
+        res(ctx.status(501), ctx.delay(timeoutDelay))
+      )
+    );
+    const { result, waitFor } = renderHook(() => useGetTags(), {
+      wrapper,
+    });
+
+    jest.runAllTimers();
+    await waitFor(() => result.current.isError, { timeout: timeoutDelay });
+    expect(result.current.error).toBeTruthy();
+  });
+});
